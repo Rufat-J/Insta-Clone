8d9bb4435ad99569bbea6019f4206e1490ee697c
Rufat-J <rj.7@live.se>
2023-11-09 15:46:46 +0100

    Added querry for user credentials crowd coding Johan, Sebastian, Rufat

--- src/main/java/com/code/instaclone/repository/UserRepository.java
+++ src/main/java/com/code/instaclone/repository/UserRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-@Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN true else false END FROM User u " +
+@Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN true else false END FROM users u " +
         "WHERE u.username = :username AND u.password = :password", nativeQuery = true)
 boolean isValidUser(String username, String password);
 
